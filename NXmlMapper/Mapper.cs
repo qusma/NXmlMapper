9f90bff0cea82c55d94ed0333b83f2fc6277ef70
qusma <qusmablog@gmail.com>
2014-03-27 16:24:51 +0200

    Comments and fixes to the mapper.

--- NXmlMapper/Mapper.cs
+++ NXmlMapper/Mapper.cs
@@ -5,6 +5,9 @@ using System.Linq;
 using System.Reflection;
 using System.Xml.Linq;
 
+// This class parses XML files and creates objects, filling property values
+// by parsing element and attribute values in the XML.
+
 namespace NXmlMapper
 {
     public class Mapper<T> where T: new()
@@ -16,7 +19,11 @@ namespace NXmlMapper
 
         private XDocument _xml;
 
-        public Mapper(string elementName = null)
+        /// <summary>
+        /// Create mapper to parse xml files to objects.
+        /// </summary>
+        /// <param name="elementName">If the element name differs from the class name, provide the target element name here.</param>
+        private Mapper(string elementName = null)
         {
             _elementPropertyMap = new Dictionary<string, string>();
             _attributePropertyMap = new Dictionary<string, string>();
@@ -27,49 +34,78 @@ namespace NXmlMapper
             GetMapsFromProperties();
         }
 
+        /// <summary>
+        /// Create mapper to parse xml files to objects.
+        /// </summary>
+        /// /// <param name="xml">The XML to be parsed.</param>
+        /// <param name="elementName">If the element name differs from the class name, provide the target element name here.</param>
         public Mapper(string xml, string elementName = null) 
             : this(elementName)
         {
             SetXml(xml);
         }
 
+        /// <summary>
+        /// Create mapper to parse xml files to objects.
+        /// </summary>
+        /// <param name="xml">The XML to be parsed.</param>
+        /// <param name="elementName">If the element name differs from the class name, provide the target element name here.</param>
         public Mapper(XDocument xml, string elementName = null)
             : this(elementName)
         {
             SetXml(xml);
         }
 
+        /// <summary>
+        /// Create mapper to parse xml files to objects.
+        /// </summary>
+        /// /// <param name="xml">The XML to be parsed.</param>
+        /// <param name="elementName">If the element name differs from the class name, provide the target element name here.</param>
         public Mapper(IEnumerable<XElement> xml, string elementName = null)
             : this(elementName)
         {
             SetXml(xml);
         }
 
+        /// <summary>
+        /// Go through the properties and look for custom mapping attributes, 
+        /// then add them to the dictionaries. In case of duplicates, the first instance holds.
+        /// </summary>
         private void GetMapsFromAttributes()
         {
             PropertyInfo[] properties = typeof(T).GetProperties();
             foreach (PropertyInfo p in properties)
             {
+                //Element -> Property mappings
                 ElementNameAttribute elementAttr = p.GetCustomAttribute<ElementNameAttribute>();
                 if (elementAttr != null) //TODO is this right?
                 {
-                    _elementPropertyMap.Add(elementAttr.ElementName, p.Name);
+                    if(!_elementPropertyMap.ContainsKey(elementAttr.ElementName))
+                        _elementPropertyMap.Add(elementAttr.ElementName, p.Name);
                 }
 
+                //Attribute -> Property mappings
                 AttributeNameAttribute attributeAttr = p.GetCustomAttribute<AttributeNameAttribute>();
                 if (attributeAttr != null) //TODO is this right?
                 {
-                    _attributePropertyMap.Add(attributeAttr.AttributeName, p.Name);
+                    if (!_attributePropertyMap.ContainsKey(attributeAttr.AttributeName))
+                        _attributePropertyMap.Add(attributeAttr.AttributeName, p.Name);
                 }
             }
         }
 
+        /// <summary>
+        /// Go through the properties, any that don't have custom mapping or the ignore attribute
+        /// are added as attribute->property mappings. 
+        /// </summary>
         private void GetMapsFromProperties()
         {
             PropertyInfo[] properties = typeof(T).GetProperties();
             foreach (PropertyInfo p in properties)
             {
-                if (p.GetCustomAttribute<ElementNameAttribute>() == null && p.GetCustomAttribute<AttributeNameAttribute>() == null)
+                if (p.GetCustomAttribute<ElementNameAttribute>() == null && 
+                    p.GetCustomAttribute<AttributeNameAttribute>() == null &&
+                    p.GetCustomAttribute<NotXmlMappedAttribute>() == null)
                 {
                     _attributePropertyMap.Add(p.Name, p.Name);
                 }
@@ -77,9 +113,9 @@ namespace NXmlMapper
         }
 
         /// <summary>
-        /// 
+        /// Set a custom mapping from a sub-element value to a property.
         /// </summary>
-        /// <param name="from">Name of the XML field.</param>
+        /// <param name="from">Name of the XML element.</param>
         /// <param name="to">Name of the class property.</param>
         public void SetElementMap(string from, string to)
         {
@@ -98,6 +134,11 @@ namespace NXmlMapper
             }
         }
 
+        /// <summary>
+        /// Set a custom mapping from an attribute value to a property.
+        /// </summary>
+        /// <param name="from">Name of the XML element.</param>
+        /// <param name="to">Name of the class property.</param>
         public void SetAttributeMap(string from, string to)
         {
             if (_attributePropertyMap.ContainsKey(from))
@@ -115,50 +156,93 @@ namespace NXmlMapper
             }
         }
 
+        /// <summary>
+        /// Set the xml input to be parsed.
+        /// </summary>
         public void SetXml(string xml)
         {
             _xml = XDocument.Load(new StringReader(xml));
         }
 
+        /// <summary>
+        /// Set the xml input to be parsed.
+        /// </summary>
         public void SetXml(XDocument xml)
         {
             _xml = xml;
         }
 
+        /// <summary>
+        /// Set the xml input to be parsed.
+        /// </summary>
         public void SetXml(IEnumerable<XElement> xml)
         {
             _xml = new XDocument(xml);
         }
 
+        /// <summary>
+        /// Parse the first matching element.
+        /// </summary>
         public T ParseOne()
         {
             if (_xml == null) throw new Exception("No XML data set");
 
-            return ParseElement(_xml.Element(_elementName)); //TODO handle null
+            //TODO maybe this should keep parsing the next element every time it's called?
+
+            XElement firstElement = _xml.Descendants(_elementName).FirstOrDefault();
+
+            if (firstElement == null)
+                throw new Exception("No matching elements found");
+
+            return ParseElement(firstElement);
         }
 
-        public List<T> ParseAll(bool recursive = false)
+        /// <summary>
+        /// Parse all matching elements.
+        /// </summary>
+        public List<T> ParseAll()
         {
             if (_xml == null) throw new Exception("No XML data set");
 
-            var list = _xml.Elements(_elementName).Select(ParseElement).ToList();
+            var list = _xml.Descendants(_elementName).Select(ParseElement).ToList(); //todo if empty?
 
             return list;
         }
 
+        /// <summary>
+        /// Parses a single XML element's attribute values and sub-element values 
+        /// into an object's properties based on the mappings.
+        /// </summary>
         private T ParseElement(XElement e)
         {
             var result = new T();
 
+            //Parse the attributes
             foreach (XAttribute attr in e.Attributes().Where(attr => _attributePropertyMap.ContainsKey(attr.Name.LocalName)))
             {
-                ParseAttribute(attr, ref result, _attributePropertyMap[attr.Name.LocalName]);
+                ParseString(attr.Value, ref result, _attributePropertyMap[attr.Name.LocalName]);
+            }
+
+            //Parse any subelements
+            foreach (string elementName in _elementPropertyMap.Keys)
+            {
+                XElement subElement = e.Element(elementName);
+                if (subElement != null)
+                {
+                    ParseString(subElement.Value, ref result, _elementPropertyMap[elementName]);
+                }
             }
 
             return result;
         }
 
-        private void ParseAttribute(XAttribute a, ref T target, string propertyName)
+        /// <summary>
+        /// Parses a string into an object's property based on the property type.
+        /// </summary>
+        /// <param name="input">String to be parsed.</param>
+        /// <param name="target">Target object.</param>
+        /// <param name="propertyName">The name of the property.</param>
+        private void ParseString(string input, ref T target, string propertyName)
         {
             PropertyInfo propertyInfo = typeof(T).GetProperty(propertyName);
             Type propertyType = propertyInfo.PropertyType;
@@ -166,7 +250,7 @@ namespace NXmlMapper
             if (propertyType == typeof(double))
             {
                 double value;
-                if (double.TryParse(a.Value, out value))
+                if (double.TryParse(input, out value))
                 {
                     propertyInfo.SetValue(target, value);
                 }
@@ -174,7 +258,7 @@ namespace NXmlMapper
             else if (propertyType == typeof(int))
             {
                 int value;
-                if (int.TryParse(a.Value, out value))
+                if (int.TryParse(input, out value))
                 {
                     propertyInfo.SetValue(target, value);
                 }
@@ -182,14 +266,14 @@ namespace NXmlMapper
             else if (propertyType == typeof(decimal))
             {
                 decimal value;
-                if (decimal.TryParse(a.Value, out value))
+                if (decimal.TryParse(input, out value))
                 {
                     propertyInfo.SetValue(target, value);
                 }
             }
             else if (propertyType == typeof(string))
             {
-                propertyInfo.SetValue(target, a.Value);
+                propertyInfo.SetValue(target, input);
             }
         }
     }
