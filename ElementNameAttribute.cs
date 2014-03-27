a6cf225f9f973b7673169aa579d6de8851818e73
qusma <qusmablog@gmail.com>
2014-03-27 04:13:30 +0200

    Added element name attribute to be used on properties that need custom mapping.

---
+++ ElementNameAttribute.cs
@@ -0,0 +1,21 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="ElementNameAttribute.cs" company="">
+// Copyright 2014 Alexander Soffronow Pagonidis
+// </copyright>
+// -----------------------------------------------------------------------
+
+using System;
+
+namespace NXmlMapper
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    public class ElementNameAttribute : Attribute
+    {
+        public string ElementName;
+
+        public ElementNameAttribute(string name)
+        {
+            ElementName = name;
+        }
+    }
+}
